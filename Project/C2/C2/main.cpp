8bb252d3dec342e5466ad8923e4401f63445f08e
HarleyHuang <harelysoa@qq.com>
2021-03-12 23:23:35 +0800

    feat: 快速排序完成

--- Project/C2/C2/main.cpp
+++ Project/C2/C2/main.cpp
@@ -18,7 +18,37 @@
 #include <iostream>
 #include "Chapter6.hpp"
 
+void swapValue(int *a, int *b);
+void quickSort(int array[], int low, int high);
 
+
+void testMethod_1();
+
+int main(int argc, const char * argv[]) {
+    
+    //    int a[] = {900, 2, -58, 3, 34, 5, 76, 7, 32, 4, 43, 9, 1, 56, 8,-70, 635, -234, 532, 543, 2500};
+    int a[] = {100, 20, 60, -20, 200};
+    int length = sizeof(a) / sizeof(int);
+    quickSort(a, 0, length - 1);
+    
+    for (int i = 0; i < 21 ; i++) {
+        printf("%d ", a[i]);
+    }
+    
+    
+    
+    
+    
+    Chapter6 chapter6;
+    //chapter6.chapter6Run();
+    
+    //testMethod_1();
+    
+    printf("\n\n");
+    system("\n pause");
+    
+    return 0;
+}
 
 void swapValue(int *a, int *b) {
     int c = *a;
@@ -30,37 +60,45 @@ void swapValue(int *a, int *b) {
 /**
  *快速排序
  */
-void quickSort(int *array[], int low, int high) {
+void quickSort(int array[], int low, int high) {
     
-    int keyValue = *array[low];
+    int keyValue = array[low];
     int i = low;
     int j = high;
     
+    //如果low >= high说明排序结束了
     if (low >= high) {
         return;
     }
     
-    while (low < high) {
-        if (keyValue >= *array[low]) {
+    while (low < high) {//该while循环结束一次表示比较了一轮 {100, 20, 60, -20, 200}
+        
+        while (low < high && array[high] >= keyValue) {
+            --high;//往后查找
+        }
+        if (array[high] < keyValue) {
+            swapValue(&(array[low]), &(array[high]));
             ++low;
-        }else {
-            while (*array[low] < keyValue) {
-                //数值进行交换
-                swapValue(array[low], array[high]);
-            }
         }
         
-        if (keyValue <= *array[high]) {
-            --high;
-        }else {
-            while (*array[high] > keyValue) {
-                swapValue(array[high], array[low]);
-            }
+        
+        while (array[low] <= keyValue && low < high) {
+            
+            ++low;
         }
+        
+        if (array[low] > keyValue) {
+            //数值进行交换
+            swapValue(&(array[low]), &(array[high]));
+            --high;
+        }
+        
         
     }
     
+    //用同样的方式对分出来的左边的部分进行同上的做法
     quickSort(array, i, low-1);
+    //用同样的方式对分出来的右边的部分进行同上的做法
     quickSort(array, low+1, j);
     
     
@@ -69,23 +107,6 @@ void quickSort(int *array[], int low, int high) {
 
 
 
-void testMethod_1();
-
-int main(int argc, const char * argv[]) {
-    
-    
-    Chapter6 chapter6;
-    
-    
-    
-    //chapter6.chapter6Run();
-    
-    //testMethod_1();
-    
-    system("pause");
-    
-    return 0;
-}
 
 void testMethod_1() {
     /*
