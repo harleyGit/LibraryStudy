353084d7bc216ad0282a9bd4045e9ebec8c01df1
Harley <harelysoa@qq.com>
2023-03-04 10:05:18 +0800

    feat: 测试weak

--- RuntimeCode/debug-objc/main.m
+++ RuntimeCode/debug-objc/main.m
@@ -7,12 +7,28 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *测试弱引用
+ *原理: https://blog.csdn.net/u013378438/article/details/82767947
+ */
+void testWeakReference() {
+    NSObject *obj = [[NSObject alloc] init];
+    __weak NSObject *weakObj = obj;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSObject *obj = [[NSObject alloc] init];
         NSLog(@"🍎 Runtime： %@",obj);
         NSLog(@"Hello, World! %@", [NSString class]);
+        
+        testWeakReference();
     }
     return 0;
 }
+
+
+
+
+
