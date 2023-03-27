8a7eb5c294540a4683a7b5b4a1c93a43088d015d
Harley <harelysoa@qq.com>
2023-03-27 19:18:56 +0800

    feat: 测试code

--- RxSwiftCode/RxSwiftCode/ViewController.swift
+++ RxSwiftCode/RxSwiftCode/ViewController.swift
@@ -8,10 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var btn: UIButton = {
+        let btn = UIButton.init(frame: CGRect.init(x: 100, y: 200, width: 200, height: 120))
+        btn.setTitle("跳转",for: UIControl.State.normal)
+        btn.backgroundColor = .red
+        //btn.addTarget(self, action:#selector(tapped(:)), for:.touchUpInside)
+        btn.addTarget(self, action: #selector(tapped), for: UIControl.Event.touchUpInside)
+       // btn.addTarget(self, action: #selector(tapped(:)), for: .touchUpInside)
+        return btn
+    }()
+    
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    @objc func tapped(sender: UIButton)  {
+//        let disposeBag = DisposeBag()
+//        let subject = AsyncSubject<String>()
+//
+//        subject.subscribe{
+//            print("subscription: 1 Event:", $0)
+//        }.disposed(by: disposeBag)
+//
+//        subject.onNext("🐩")
+//        subject.onNext("🐶")
+//        subject.onNext("🐱")
+//        subject.onNext("🥜")
+//        subject.onCompleted()
     }
 
 
